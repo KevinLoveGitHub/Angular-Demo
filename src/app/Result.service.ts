e738a8c1f305cde983b3d52615737dc956cd60bf
Kevin <616651408@qq.com>
2017-08-23 17:34:59 +0800

    Update slider

---
+++ src/app/Result.service.ts
@@ -0,0 +1,5 @@
+
+// export class ResultService {
+//   private resultUrl = 'http://gank.io/api/data/%E7%A6%8F%E5%88%A9/5/1';
+//
+// }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,29 @@ import {Component} from '@angular/core';
 import {Gank} from './Gank';
 import {Result} from './Result';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: '<h1 (click)="toPhoto()">{{title}}</h1>'
 })
 
 export class AppComponent {
   title = 'app';
-  gank4 = new Gank('http://valor-software.com/ngx-bootstrap/assets/images/nature/4.jpg');
-  gank5 = new Gank('http://valor-software.com/ngx-bootstrap/assets/images/nature/5.jpg');
-  gank6 = new Gank('http://valor-software.com/ngx-bootstrap/assets/images/nature/6.jpg');
-  list: Array<Gank> = [this.gank4, this.gank5, this.gank6];
+  gank1 = new Gank('https://ws1.sinaimg.cn/large/610dc034ly1fitcjyruajj20u011h412.jpg');
+  gank2 = new Gank('https://ws1.sinaimg.cn/large/610dc034ly1fis7dvesn6j20u00u0jt4.jpg');
+  gank3 = new Gank('https://ws1.sinaimg.cn/large/610dc034ly1fil82i7zsmj20u011hwja.jpg');
+  gank4 = new Gank('https://ws1.sinaimg.cn/large/610dc034ly1fik2q1k3noj20u00u07wh.jpg');
+  gank5 = new Gank('https://ws1.sinaimg.cn/large/610dc034ly1fir1jbpod5j20ip0newh3.jpg');
+  list: Array<Gank> = [this.gank1, this.gank2, this.gank3, this.gank4, this.gank5];
   data = new Result('123', this.list);
 
   toPhoto(): void {
     const temp = this;
-    this.title = 'title';
+    temp.title = 'title';
     console.log(temp.data);
   }
+
 }
 
 
