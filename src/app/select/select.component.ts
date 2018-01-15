3a4940e79cc30e61032c8b553af04a0b0ed1f7fa
Kevin <616651408@qq.com>
2018-01-15 10:44:47 +0800

    Add save del options

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -22,6 +22,7 @@ export class SelectComponent implements OnInit {
   rootMarginLeft: number;
   viewHeight: number;
   views = [];
+  optionViewHeight: number;
 
   constructor(private renderer: Renderer2, private el: ElementRef) {
   }
@@ -30,6 +31,7 @@ export class SelectComponent implements OnInit {
     this.rootWidth = 1800 * this.zoom;
     this.rootHeight = 800 * this.zoom;
     this.viewHeight = 160;
+    this.optionViewHeight = 120;
     this.rootMarginLeft = (innerWidth - this.rootWidth) / 2;
     this.rowValue = this.rootWidth / this.row.length;
     this.columnValue = this.rootHeight / this.column.length;
@@ -37,38 +39,38 @@ export class SelectComponent implements OnInit {
     const view1 = {
       id: '1',
       img: './assets/clock.jpg',
-      width : '600',
-      height : '800'
+      width: '600',
+      height: '800'
     };
     const view2 = {
       id: '2',
       img: './assets/clock.jpg',
-      width : '400',
-      height : '300'
+      width: '400',
+      height: '300'
     };
     const view3 = {
       id: '3',
       img: './assets/clock.jpg',
-      width : '400',
-      height : '300'
+      width: '400',
+      height: '300'
     };
     const view4 = {
       id: '4',
       img: './assets/clock.jpg',
-      width : '800',
-      height : '500'
+      width: '800',
+      height: '500'
     };
     const view5 = {
       id: '5',
       img: './assets/clock.jpg',
-      width : '400',
-      height : '400'
+      width: '400',
+      height: '400'
     };
     const view6 = {
       id: '6',
       img: './assets/clock.jpg',
-      width : '400',
-      height : '400'
+      width: '400',
+      height: '400'
     };
 
     this.views.push(view1, view2, view3, view4, view5, view6);
@@ -76,10 +78,20 @@ export class SelectComponent implements OnInit {
 
 
   selectView(event: any) {
+    const target = event.target;
+    // 判断选中是否当前选中组件
+    if (this.selectedView != null && this.selectedView.id === target.id) {
+      return;
+    }
+    // 置空当前选中组件的border
     if (this.selectedView != null) {
       this.selectedView.style.border = null;
     }
-    this.selectedView = event.target;
+    // 置空已经放置的组件对象
+    if (this.hasPutView != null) {
+      this.hasPutView = null;
+    }
+    this.selectedView = target;
     this.selectedView.style.border = 'aqua solid 10px';
     console.log(this.selectedView);
 
@@ -115,8 +127,12 @@ export class SelectComponent implements OnInit {
     const marginLeft = event.layerX;
     const marginTop = event.layerY;
 
-    const width = Number(style.width.slice(0, -2)) * 2 + 'px';
-    const height = Number(style.height.slice(0, -2)) * 2 + 'px';
+    const view = this.views.find(v => v.id === this.selectedView.id);
+    const width = view.width * this.zoom + 'px';
+    const height = view.height * this.zoom + 'px';
+
+    // const width = Number(style.width.slice(0, -2)) * 2 + 'px';
+    // const height = Number(style.height.slice(0, -2)) * 2 + 'px';
     if (this.hasPutView == null) {
       this.hasPutView = this.createView(this.selectedView, marginLeft, marginTop);
     } else {
@@ -127,18 +143,21 @@ export class SelectComponent implements OnInit {
   }
 
   moveView(view: any, marginLeft: any, marginTop: any) {
-    console.log(view);
     marginLeft = this.getMarginLeft(marginLeft);
     marginTop = this.getMarginTop(marginTop);
     view.style.marginLeft = marginLeft;
     view.style.marginTop = marginTop;
   }
 
-  createView(view: any, marginLeft: any, marginTop: any): ElementRef {
-    const srcUrl = view.currentSrc;
-    const style = view.style;
-    const width = Number(style.width.slice(0, -2)) * 4.5 + 'px';
-    const height = Number(style.height.slice(0, -2)) * 4.5 + 'px';
+  createView(selectedView: any, marginLeft: any, marginTop: any): ElementRef {
+    const srcUrl = selectedView.currentSrc;
+    const style = selectedView.style;
+    const view = this.views.find(v => v.id === selectedView.id);
+    const width = view.width * this.zoom + 'px';
+    const height = view.height * this.zoom + 'px';
+
+    // const width = Number(style.width.slice(0, -2)) * 4.5 + 'px';
+    // const height = Number(style.height.slice(0, -2)) * 4.5 + 'px';
     // const leftRemainder = marginLeft % 50;
     // marginLeft = marginLeft - leftRemainder;
 
@@ -153,26 +172,23 @@ export class SelectComponent implements OnInit {
     const div = this.renderer.createElement('div');
     const img = this.renderer.createElement('img');
     this.renderer.setAttribute(img, 'src', srcUrl);
-    this.renderer.setStyle(
-      img, 'width', width
-    );
-    this.renderer.setStyle(
-      img, 'height', height
-    );
-    this.renderer.setStyle(
-      img, 'margin-left', marginLeft
-    );
-    this.renderer.setStyle(
-      img, 'margin-top', marginTop
-    );
+    const imgStyle = img.style;
+    imgStyle.width = width;
+    imgStyle.height = height;
+    imgStyle.position = 'absolute';
+    div.style.marginLeft = marginLeft;
+    div.style.marginTop = marginTop;
+    div.style.position = 'absolute';
 
-    this.renderer.setStyle(
-      img, 'position', 'absolute'
-    );
+    // 设置click事件
+    this.renderer.listen(img, 'click', function (event) {
+      console.log(event);
+    });
 
     this.renderer.appendChild(div, img);
+    this.renderer.appendChild(div, this.getOptionsView(div, width, height, marginTop, marginLeft));
     this.renderer.appendChild(this.root.nativeElement, div);
-    return img;
+    return div;
   }
 
   getMarginLeft(marginLeft: any): string {
@@ -185,6 +201,59 @@ export class SelectComponent implements OnInit {
     const topRemainder = marginTop % 90;
     marginTop = marginTop - topRemainder;
     return marginTop + 'px';
+  }
+
+  getOptionsView(view: any, width: string, height: string, marginTop: string, marginLeft: string): ElementRef {
+    const div = this.renderer.createElement('div');
+    const confirmDiv = this.renderer.createElement('div');
+    const confirmImg = this.renderer.createElement('img');
+    const cancelDiv = this.renderer.createElement('div');
+    const cancelImg = this.renderer.createElement('img');
+
+    const divStyle = div.style;
+    marginTop = Number(height.slice(0, -2)) - this.optionViewHeight + 'px';
+    divStyle.width = width;
+    divStyle.height = this.optionViewHeight + 'px';
+    divStyle.marginTop = marginTop;
+    divStyle.position = 'absolute';
+    divStyle.display = 'flex';
+    divStyle.flexDirection = 'row';
+    divStyle.justifyContent = 'space-between';
+    divStyle.alignItems = 'center';
+    divStyle.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+
+    const confirmStyle = confirmDiv.style;
+    confirmStyle.height = '100%';
+    confirmStyle.width = '50%';
+    confirmStyle.display = 'flex';
+    confirmStyle.justifyContent = 'center';
+    confirmStyle.alignItems = 'center';
+    confirmImg.src = './assets/confirm.png';
+
+    const cancelStyle = cancelDiv.style;
+    cancelStyle.height = '100%';
+    cancelStyle.width = '50%';
+    cancelStyle.display = 'flex';
+    cancelStyle.justifyContent = 'center';
+    cancelStyle.alignItems = 'center';
+    cancelImg.src = './assets/cancel.png';
+
+    this.renderer.appendChild(cancelDiv, cancelImg);
+    this.renderer.appendChild(confirmDiv, confirmImg);
+    this.renderer.appendChild(div, cancelDiv);
+    this.renderer.appendChild(div, confirmDiv);
+
+    this.renderer.listen(cancelDiv, 'click', () => {
+      this.renderer.removeChild(view.parent, view);
+      this.hasPutView = null;
+    });
+    this.renderer.listen(confirmDiv, 'click', event => {
+      div.style.display = 'none';
+      this.hasPutView = null;
+      this.selectedView.style.border = null;
+      this.selectedView = null;
+    });
+    return div;
   }
 
 
@@ -236,60 +305,61 @@ export class SelectComponent implements OnInit {
   //   this.renderer.appendChild(this.root.nativeElement, div);
   // }
 
-  createGuide(parentView: any, width: string, height: string) {
-    const viewWidth = Number(width.slice(0, -2));
-    const viewHeight = Number(height.slice(0, -2));
-    const topDiv = this.renderer.createElement('div');
-    const top = this.renderer.createElement('img');
-    this.renderer.appendChild(topDiv, top);
-    this.renderer.setAttribute(top, 'src', 'assets/top.png');
-    this.renderer.setStyle(
-      topDiv, 'position', 'absolute'
-    );
-    this.renderer.setStyle(
-      top, 'width', viewWidth / 3 + 'px'
-    );
-    this.renderer.setStyle(
-      top, 'height', viewHeight / 3 + 'px'
-    );
+  // createGuide(parentView: any, width: string, height: string) {
+  //   const viewWidth = Number(width.slice(0, -2));
+  //   const viewHeight = Number(height.slice(0, -2));
+  //   const topDiv = this.renderer.createElement('div');
+  //   const top = this.renderer.createElement('img');
+  //   this.renderer.appendChild(topDiv, top);
+  //   this.renderer.setAttribute(top, 'src', 'assets/top.png');
+  //   this.renderer.setStyle(
+  //     topDiv, 'position', 'absolute'
+  //   );
+  //   this.renderer.setStyle(
+  //     top, 'width', viewWidth / 3 + 'px'
+  //   );
+  //   this.renderer.setStyle(
+  //     top, 'height', viewHeight / 3 + 'px'
+  //   );
+  //
+  //   this.renderer.setStyle(
+  //     top, 'margin-left', (viewWidth - viewWidth / 3) / 2 + 'px'
+  //   );
+  //
+  //   const bottomDiv = this.renderer.createElement('div');
+  //   const bottom = this.renderer.createElement('img');
+  //   this.renderer.setAttribute(bottom, 'src', 'assets/bottom.png');
+  //
+  //   this.renderer.appendChild(bottomDiv, bottom);
+  //   this.renderer.setStyle(
+  //     bottomDiv, 'position', 'absolute'
+  //   );
+  //   this.renderer.setStyle(
+  //     bottom, 'width', viewWidth / 3 + 'px'
+  //   );
+  //   this.renderer.setStyle(
+  //     bottom, 'height', viewHeight / 3 + 'px'
+  //   );
+  //
+  //   this.renderer.setStyle(
+  //     bottom, 'margin-top', (viewHeight - viewHeight / 3) + 'px'
+  //   );
+  //
+  //   this.renderer.setStyle(
+  //     bottom, 'margin-left', (viewWidth - viewWidth / 3) / 2 + 'px'
+  //   );
+  //
+  //   const leftDiv = this.renderer.createElement('div');
+  //   const left = this.renderer.createText('左');
+  //   this.renderer.appendChild(leftDiv, left);
+  //
+  //   const rightDiv = this.renderer.createElement('div');
+  //   const right = this.renderer.createText('右');
+  //   this.renderer.appendChild(rightDiv, right);
+  //
+  //   this.renderer.appendChild(parentView, topDiv);
+  //   this.renderer.appendChild(parentView, bottomDiv);
+  // }
 
-    this.renderer.setStyle(
-      top, 'margin-left', (viewWidth - viewWidth / 3) / 2 + 'px'
-    );
-
-    const bottomDiv = this.renderer.createElement('div');
-    const bottom = this.renderer.createElement('img');
-    this.renderer.setAttribute(bottom, 'src', 'assets/bottom.png');
-
-    this.renderer.appendChild(bottomDiv, bottom);
-    this.renderer.setStyle(
-      bottomDiv, 'position', 'absolute'
-    );
-    this.renderer.setStyle(
-      bottom, 'width', viewWidth / 3 + 'px'
-    );
-    this.renderer.setStyle(
-      bottom, 'height', viewHeight / 3 + 'px'
-    );
-
-    this.renderer.setStyle(
-      bottom, 'margin-top', (viewHeight - viewHeight / 3) + 'px'
-    );
-
-    this.renderer.setStyle(
-      bottom, 'margin-left', (viewWidth - viewWidth / 3) / 2 + 'px'
-    );
-
-    const leftDiv = this.renderer.createElement('div');
-    const left = this.renderer.createText('左');
-    this.renderer.appendChild(leftDiv, left);
-
-    const rightDiv = this.renderer.createElement('div');
-    const right = this.renderer.createText('右');
-    this.renderer.appendChild(rightDiv, right);
-
-    this.renderer.appendChild(parentView, topDiv);
-    this.renderer.appendChild(parentView, bottomDiv);
-  }
 
 }
